a8b41751a0160c24467030a825b1cf3933775d31
Jae <comea22@gmail.com>
2025-12-22 23:08:52 +0800

    feat: 新增文章分類按鈕

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	type: 'content',
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+			category: z.enum(['前端', '後端', 'UIUX', '心得分享']).optional(),
+		}),
+});
+
+export const collections = { blog };
