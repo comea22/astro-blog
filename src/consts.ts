ad2abc183de52d616144a7cd13bf0d649afa5a9f
Jae <comea22@gmail.com>
2025-12-27 14:10:47 +0800

    chore: 更新網站描述與社群分享圖片

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '程式散步｜學習筆記';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '網頁開發的學習旅程';
