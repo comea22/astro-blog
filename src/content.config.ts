a00b0be6a4c50913c561f06d3dfd3e1243948262
Jae Liang <jae@JaedeMacBook-Air.local>
2026-03-01 22:36:45 +0800

    chore: 新增文章

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,13 +3,12 @@ import { glob } from 'astro/loaders';
 
 const notes = defineCollection({
 	loader: glob({ base: './src/content/notes', pattern: '**/*.{md,mdx}' }),
-	schema: ({ image }) =>
-		z.object({
+	schema: z.object({
 			title: z.string(),
 			description: z.string(),
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
+			heroImage: z.string().optional(),
 			draft: z.boolean().optional(),
 			category: z.string().optional(),
 		}),
